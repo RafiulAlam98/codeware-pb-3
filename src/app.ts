5d700e96adb40c6b0de7087fedbde2e08c3140f1
RafiulAlam <mrafiul.alam7@gmail.com>
2023-07-05 00:05:02 +0600

    initialized the project

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, { Application, NextFunction, Request, Response } from 'express'
+
+import cors from 'cors'
+import httpStatus from 'http-status'
+
+const app: Application = express()
+
+//cors
+app.use(cors())
+
+//parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//Application Route
+
+
+// Testing
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//   throw new ApiError(100, 'hi', '')
+//   //   next()
+// })
+
+// app.use(globalErrorHandler)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessage: [
+      {
+        path: req.originalUrl,
+        message: 'Api Not Found',
+      },
+    ],
+    stack: '',
+  })
+  next()
+})
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import { Server } from "http"
+import app from "./app"
+import config from "./config"
+import mongoose from "mongoose"
+
+let server: Server
+
+process.on('uncaughtException', err => {
+  console.log(err)
+  process.exit(1)
+})
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+   console.log('database connected')
+    app.listen(config.port, () => {
+     console.log(`listening on port ${config.port}`)
+    })
+  } catch (err) {
+   console.log('Failed to conncet', err)
+  }
+  process.on('unhandledRejection', error => {
+    console.log('Unhandled rejection, We are closing our server')
+    if (server) {
+      server.close(() => {
+        console.log(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
+}
+
+main()
